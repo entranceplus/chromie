2ea2c6a051c0919ee46938322a48b48f8146b539
ravi8470 <rvkmr0851@gmail.com>
2019-04-11 01:48:54 +0530

    added scheduler for heroku

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('make:file')->everyMinute();
+        $schedule->command('link:diff')->everyMinute();
     }
 
     /**
