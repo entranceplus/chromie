3f4886db3a03abf2ada96030562f5ce5c8812fc5
ravi8470 <rvkmr0851@gmail.com>
2019-04-16 05:02:04 +0530

    prevented laravel from caching views

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('make:file')->everyMinute();
+        $schedule->command('view:clear')->everyMinute();
         $schedule->command('link:diff')->everyMinute();
     }
 
